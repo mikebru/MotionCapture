a18c90ca8cb9cfc7178e541799cbca2a61708753
michaelbruner <michaelbruner@tamu.edu>
2018-07-19 13:01:25 -0500

    Initial commit

---
+++ Assets/Plugins/RootMotion/FinalIK/_DEMOS/VRIK/Scripts/VRIKCalibrationController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using RootMotion.FinalIK;
+
+namespace RootMotion.Demos
+{
+
+    public class VRIKCalibrationController : MonoBehaviour
+    {
+
+        [Tooltip("Reference to the VRIK component on the avatar.")] public VRIK ik;
+		[Tooltip("The settings for VRIK calibration.")] public VRIKCalibrator.Settings settings;
+        [Tooltip("The HMD.")] public Transform headTracker;
+		[Tooltip("(Optional) A tracker placed anywhere on the body of the player, preferrably close to the pelvis, on the belt area.")] public Transform bodyTracker;
+		[Tooltip("(Optional) A tracker or hand controller device placed anywhere on or in the player's left hand.")] public Transform leftHandTracker;
+		[Tooltip("(Optional) A tracker or hand controller device placed anywhere on or in the player's right hand.")] public Transform rightHandTracker;
+		[Tooltip("(Optional) A tracker placed anywhere on the ankle or toes of the player's left leg.")] public Transform leftFootTracker;
+		[Tooltip("(Optional) A tracker placed anywhere on the ankle or toes of the player's right leg.")] public Transform rightFootTracker;
+
+		void Awake()
+		{
+			//find an IK if one is not assigned
+			if (ik == null) {
+				ik = FindObjectOfType<VRIK> ();
+			}
+
+			RenamePoints ();
+		}
+
+		void RenamePoints()
+		{
+			headTracker.name = ik.references.head.name;
+			bodyTracker.name = ik.references.pelvis.name;
+			leftHandTracker.name = ik.references.leftHand.name;
+			rightHandTracker.name = ik.references.rightHand.name;
+			leftFootTracker.name = ik.references.leftFoot.name;
+			rightFootTracker.name = ik.references.rightFoot.name;
+
+		}
+			
+        void LateUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                Calibrate();
+            }
+        }
+
+        [ContextMenu("Calibrate")]
+        void Calibrate() {
+			VRIKCalibrator.Calibrate(ik, settings, headTracker, bodyTracker, leftHandTracker, rightHandTracker, leftFootTracker, rightFootTracker);
+        }
+    }
+}
---
+++ Assets/SteamVR/Scripts/TrackerSerial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class TrackerSerial : MonoBehaviour {
+
+    public string SerialNumber;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+
+}
+
+
